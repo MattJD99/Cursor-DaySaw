cde7eea0316740494562e0041b7d6520bfc3edab
DaySaw Agency <daysawagency@macs-mac-mini.mynetworksettings.com>
2025-02-01 23:47:57 -0800

    Update: Adjust video position, remove pink gradients, unify header styling, fix footer dots

--- components/ui/DotsBackground.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function DotsBackground() {
-  return (
-    <div className="relative w-full overflow-hidden bg-gradient-to-b from-white via-pink-50/50 to-white py-8">
-      <div className="w-full h-12 bg-[url('/images/googlefi-dots.webp')] bg-repeat-x bg-contain" />
-    </div>
-  );
-} 
